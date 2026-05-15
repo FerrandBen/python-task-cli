bdb5dd99c8077ec420e127e24dff5410b52db7cf
benjamin <ferrandbenjamin33@gmail.com>
2026-05-15 18:05:59 +0200

    feat : add JSON save and load + reformat task_manager.py

---
+++ task_manager/storage.py
@@ -0,0 +1,22 @@
+import json
+
+from task import Task
+import os
+
+FILE_PATH = "data/tasks.json"
+
+def save_tasks(task_list):
+
+    data = [task.to_dict() for task in task_list]
+
+    with open(FILE_PATH, "w") as file:
+        json.dump(data, file)
+
+
+def load_tasks():
+    if os.path.exists(FILE_PATH):
+        with open(FILE_PATH, "r") as file:
+            data = json.load(file)
+        return [Task.from_dict(item) for item in data]
+    else:
+        return []
--- task_manager/task_manager.py
+++ task_manager/task_manager.py
@@ -1,6 +1,7 @@
 from task import Task
 
-class Task_manager():
+
+class Task_manager:
 
     def __init__(self):
         self.task_list: list = []
@@ -12,17 +13,17 @@ class Task_manager():
         self.task_list.append(newTask)
 
         return newTask
-    
+
     def delete_task(self, id):
 
         deleteTask = self.search_by_id(id)
 
         return self.task_list.remove(deleteTask)
-    
+
     def show_tasks(self):
 
         return self.task_list
-    
+
     def search_by_id(self, id):
 
         for task in self.task_list:
