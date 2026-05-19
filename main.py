6fe84007e5ac07afac1a460ded9fc8dc68963c09
FerrandBen <ferrandbenjamin33@gmail.com>
2026-05-19 11:55:53 +0200

    fix : some reformat

--- main.py
+++ main.py
@@ -3,29 +3,30 @@ from task_manager.storage import save_tasks, load_tasks
 import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='A simple Task Manager')
-    parser.add_argument("choix",choices=['add', 'list', 'done', 'delete'],
-                    help="Choix possibles")
+    parser = argparse.ArgumentParser(description="A simple Task Manager")
+    parser.add_argument(
+        "choix", choices=["add", "list", "done", "delete"], help="Choix possibles"
+    )
     parser.add_argument("valeur", nargs="?", help="Titre ou id selon la commande")
     args = parser.parse_args()
 
     manager = Task_manager()
     manager.task_list = load_tasks()
-    if args.choix == 'add':
+    if args.choix == "add":
         result = manager.add_task(args.valeur)
         save_tasks(manager.task_list)
         print("La tâche a bien été ajoutée")
-    elif args.choix == 'list':
+    elif args.choix == "list":
         print("Voici la liste des tâches :")
         result = manager.show_tasks()
         for task in manager.show_tasks():
             print(task)
-    elif args.choix == 'done':
+    elif args.choix == "done":
         result = manager.search_by_id(args.valeur)
         result.mark_as_done()
         print("La tâche a bien été marqué comme terminé")
         save_tasks(manager.task_list)
-    elif args.choix == 'delete':
+    elif args.choix == "delete":
         result = manager.delete_task(args.valeur)
         save_tasks(manager.task_list)
         print("La tâche a bien été supprimée")
